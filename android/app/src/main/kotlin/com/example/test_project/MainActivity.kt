d6c0df61e46a79e793d2c57452466185a566aad8
Shweta Rajput <shwetakashyap942001@gmail.com>
2025-01-24 15:01:43 +0530

    MainActivity.kt

--- android/app/src/main/kotlin/com/example/test_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_project/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.test_project
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.tflite.TflitePlugin
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Automatically register plugins
+        GeneratedPluginRegistrant.registerWith(flutterEngine!!)
+
+        // Initialize the Tflite Plugin
+        TflitePlugin().registerWith(registrarFor("io.flutter.plugins.tflite.TflitePlugin"))
+    }
+}
